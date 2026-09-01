510d35b4496f2658a48cc66a54978712a74d9b02
Omm18A <ommprakashnayak1520@gmail.com>
2026-09-01 11:34:13 +0530

    Add: first-missing-positive - submission-5

---
+++ Data Structures & Algorithms/first-missing-positive/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        HashSet<Integer>a=new HashSet<>();
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            a.add(nums[i]);
+            max=Math.max(max,nums[i]); 
+        }
+        for(int i=1;i<max;i++){
+            if(!a.contains(i)){
+                return i;
+            }
+        }
+        return max+1;
+    }
+}
