70898c45e83093a7ee49fe5bb8457bf8ffacd4a8
Omm18A <ommprakashnayak1520@gmail.com>
2026-09-05 18:58:38 +0530

    Add: sqrtx - submission-6

---
+++ Data Structures & Algorithms/sqrtx/submission-6.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        int max=0;
+        if(x==0){
+            return 0;
+        }
+        if(x==1){
+            return 1;
+        }
+        for(int i=1;i<=Math.sqrt(x);i++){
+            if(i*i<=x){
+                max=i;
+            }
+        }
+       return max; 
+    }
+}
